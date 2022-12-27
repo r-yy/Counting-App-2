bd3f37ba65596d0e1b8e56f35e14a7a5ee2006cc
Ramil Yanberdin <r-yy@outlook.com>
2022-12-27 20:04:28 +0300

    Upload my Counting App

--- Counting App/ViewController.swift
+++ Counting App/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var increaseButton: UIButton!
+    private var counter: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        counterLabel.text = "0"
     }
 
-
+    @IBAction func action(_ sender: Any) {
+        counter += 1
+        counterLabel.text = String(counter)
+    }
 }
 

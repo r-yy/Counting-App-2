e9de87e064324ed7ee2c8976000dc55270492a50
Ramil Yanberdin <r-yy@outlook.com>
2022-12-27 19:21:00 +0300

    Initial Commit

---
+++ Counting App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counting App
+//
+//  Created by Ramil Yanberdin on 27.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Counting AppUITests/Counting_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Counting_AppUITestsLaunchTests.swift
+//  Counting AppUITests
+//
+//  Created by Ramil Yanberdin on 27.12.2022.
+//
+
+import XCTest
+
+final class Counting_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
